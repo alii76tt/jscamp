c05133c30653817eebbe827495d566bd7cd5bfb1
alii76tt <ali.iltizar@icloud.com>
2021-10-19 00:13:28 +0300

    end of october 18

---
+++ src/app.js
@@ -0,0 +1,49 @@
+// JS type safe degildir
+
+
+
+console.log("hello world!") //ekrana yzdırma komutu
+
+// degisken tanımlama       
+
+/*var dolarYesterday = 9.20;
+var dolarToday = 9.30;*/
+
+// var yerine let kullan
+
+
+let dolarYesterday = 9.20;
+let dolarToday = 9.20;
+
+{
+    let dolarYesterday = 9.10;
+}
+
+console.log(dolarYesterday);
+
+// const> sabit demek
+
+
+/*const euroYesterday = 11.2
+euroYesterday =2
+
+boyle kullanılmaz
+*/
+
+// diziler
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"];
+
+// boylede yazılabilir
+//let konutKredileriFarkli = ["Konut Kredisi", 1, ["a", "b"]];
+
+// gelen veriyi gormek icin 
+console.log(konutKredileri);
+
+
+console.log("Krediler");
+for (let i = 0; i < konutKredileri.length; i++) {
+
+    console.log(konutKredileri[i]);
+    
+}
