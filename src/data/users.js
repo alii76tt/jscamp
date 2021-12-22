bf5713b11d2e6226d8926ad8a0db21a452df543d
Ali <iltoxzero@gmail.com>
2021-12-22 15:49:37 +0300

    fourth lesson

---
+++ src/data/users.js
@@ -0,0 +1,42 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Ali",
+        lastName: "Iltizar",
+        age: 25,
+        creditCardNumber: "987654",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        age: 36,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Mustafa",
+        lastName: "Kaya",
+        age: "asfea29a",
+        salary: 8000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Cansu",
+        lastName: "Sansar",
+        //age: 21,
+        salary: 8500,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Eren",
+        //lastName: "Arı",
+        age: 21,
+        salary: 9500,
+        type: "employe"
+    }
+]
